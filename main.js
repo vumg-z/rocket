9f9725b8e16edd28a93bf176959d78304172a410
vumg <ab17100222@gmail.com>
2023-05-27 00:02:25 -0600

    propulsion system

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from './OrbitControls.js';
+import * as CANNON from 'cannon';
+let world, propulsionZoneBody, boxBodies = [];
+
 
 let camera, scene, renderer, controls;
 
@@ -36,6 +39,44 @@ function init() {
 
     // Crear los controles de la cámara
     controls = new OrbitControls(camera, renderer.domElement);
+
+    // Configurar el mundo físico
+    world = new CANNON.World();
+    world.gravity.set(0, -9.82, 0); // Gravedad en la dirección -y
+
+    // Crear la referencia de la posición de generación de las cajas
+    const boxGenerationPosition = new THREE.Vector3(0, -0.25, 0);
+
+    // Configurar cuerpo físico de propulsionZone
+    propulsionZoneBody = new CANNON.Body({
+        mass: 0, // mass == 0 hace que el cuerpo sea estático
+        position: new CANNON.Vec3(propulsionZone.position.x, propulsionZone.position.y, propulsionZone.position.z),
+        shape: new CANNON.Box(new CANNON.Vec3(1, 0.25, 1)) // los parámetros son la mitad de los tamaños en x, y, z
+    });
+    world.addBody(propulsionZoneBody);
+
+    // Crear y añadir cuerpos de caja justo debajo de la posición de generación
+    for (let i = 0; i < 10; i++) {
+        createBoxAtPosition(boxGenerationPosition.x, boxGenerationPosition.y, boxGenerationPosition.z);
+    }
+}
+
+// Función para crear una caja en una posición específica
+function createBoxAtPosition(x, y, z) {
+    let box = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 0.5),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    );
+    box.position.set(x + (Math.random() * 2 - 1), y, z + (Math.random() * 2 - 1)); // Posiciones aleatorias alrededor de la posición dada
+    scene.add(box);
+
+    let boxBody = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(box.position.x, box.position.y, box.position.z),
+        shape: new CANNON.Box(new CANNON.Vec3(0.25, 0.25, 0.25))
+    });
+    world.addBody(boxBody);
+    boxBodies.push({ threeObject: box, cannonBody: boxBody });
 }
 
 function animate() {
@@ -43,6 +84,34 @@ function animate() {
 
     // Actualizar los controles de la cámara
     controls.update();
+
+    // Avanzar la simulación física
+    world.step(1 / 60); // El argumento es el tiempo en segundos desde la última vez que se llamó a world.step
+
+    // Actualizar posiciones de los cuadros en Three.js para coincidir con Cannon.js
+    for (let box of boxBodies) {
+        box.threeObject.position.copy(box.cannonBody.position);
+        box.threeObject.quaternion.copy(box.cannonBody.quaternion);
+    }
+
+    // Generar más cajas de forma continua justo debajo de la posición de generación
+    if (boxBodies.length < 100) { // Limitar el número total de cajas para evitar el agotamiento de recursos
+        createBoxAtPosition(0, -0.25, 0);
+    }
+
+    // Eliminar cajas que están por debajo de una cierta distancia
+    const distanceThreshold = -10; // Ajusta esto a la distancia que desees
+    for (let i = boxBodies.length - 1; i >= 0; i--) {
+        if (boxBodies[i].threeObject.position.y < distanceThreshold) {
+            // Eliminar de la escena Three.js
+            scene.remove(boxBodies[i].threeObject);
+            // Eliminar del mundo Cannon.js
+            world.remove(boxBodies[i].cannonBody);
+            // Eliminar de la lista boxBodies
+            boxBodies.splice(i, 1);
+        }
+    }
+
 
     // Renderizar la escena
     renderer.render(scene, camera);
