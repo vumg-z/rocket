09471b7aa842ee632fb1af4c514a6b7f0f7447bd
vumg <ab17100222@gmail.com>
2023-05-26 23:51:23 -0600

    version stable

--- main.js
+++ main.js
@@ -1,89 +1,106 @@
 import * as THREE from 'three';
-import CubeConnectorStructure from './cubes/cubeConnectorStructure.js';
-import CameraController from './camera.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import CameraController from './camera';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x808080); // Set to gray
-document.body.appendChild(renderer.domElement);
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-
-document.body.appendChild(renderer.domElement);
-
-const spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(0, 5, 10);
-spotLight.castShadow = true;
-scene.add(spotLight);
-
-spotLight.shadow.mapSize.width = 1024; // tamaño de la textura de la sombra
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 0.5; // cuán cerca comienza a proyectarse la sombra
-spotLight.shadow.camera.far = 500; // cuán lejos se proyecta la sombra
+let camera, scene, renderer, cameraController;
 
 
-// Parámetros para los Cubos y el Conector
-const cubeSize = 1; // Cube size
-const connectorSize = .9; // Connector size
-
-const cube1Color = 0x000000; // Cube color (in hexadecimal format)
-const cube2Color = 0x000000; // Cube color (in hexadecimal format)
-const connectorColor = 0xffffff; // Connector color (in hexadecimal format)
-
-// Crear una matriz 2x2 de CubeConnectorStructure
-const structures = [];
-
-const positions = [
-    { x: 0, y: 1.999, z: 0.23931271456177017 },
-    { x: 1.0961179061289288, y: 2, z: -0.8602099121356961 },
-    { x: 1.0961179061289288, y: 2, z: 0.23931271456177017 },
-    { x: -0.0052521226297396195, y: 2, z: -0.8700423185801522 },
-];
-
-
-for (let i = 0; i < positions.length; i++) {
-    const connectorPosition = positions[i]; // Posición del Conector
-    const cube1Props = { size: cubeSize, color: cube1Color, position: { x: connectorPosition.x, y: connectorPosition.y + 0.6, z: connectorPosition.z } };
-    const cube2Props = { size: cubeSize, color: cube2Color, position: { x: connectorPosition.x, y: connectorPosition.y - 0.6, z: connectorPosition.z } };
-    const connectorProps = { size: connectorSize, color: connectorColor, position: connectorPosition };
-    const structure = new CubeConnectorStructure(cube1Props, cube2Props, connectorProps);
-    structure.addToScene(scene);
-    structures.push(structure);
-}
-
-
-camera.position.z = 5;
-
-let time = 0;
-
-function animate() {
-	time += 0.05; // adjust speed of oscillation
-
-	// Move both cubes up and down between their respective limits
-	const deltaY = Math.sin(time) * 0.15;
-
-	for (let structure of structures) {
-		structure.moveCubes(deltaY);
-	}
-
-	renderer.render(scene, camera);
-	requestAnimationFrame(animate);
-}
-
+init();
 animate();
 
-// Create an instance of CameraController and pass it the camera and the DOM element
-const cameraController = new CameraController(camera, renderer.domElement);
+function init() {
+    // Crear la cámara
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(-5.206487822512527, 0.3727371290852184, 14.501602931402532);
+    const euler = new THREE.Euler(0, 0.5499999999999999, 0, 'XYZ');
+    camera.setRotationFromEuler(euler);
 
-// Render the scene
-function render() {
-	cameraController.update(); // Update the camera movement
-	renderer.render(scene, camera);
-	requestAnimationFrame(render);
+
+    // Crear la escena
+    scene = new THREE.Scene();
+
+    // Crear el loader de MTLLoader
+    const mtlLoader = new MTLLoader();
+
+    // Cargar el archivo .mtl y el archivo .obj
+    mtlLoader.load(
+        // URL del archivo .mtl
+        'rocket/rocket.mtl',
+        function (materials) {
+            materials.preload();
+
+            // Crear el loader de OBJLoader
+            const objLoader = new OBJLoader();
+
+            // Asignar los materiales cargados al loader de OBJLoader
+            objLoader.setMaterials(materials);
+
+            // Cargar el archivo .obj
+            objLoader.load(
+                // URL del archivo .obj
+                'rocket/rocket.obj',
+                function (rocket) {
+                    // Obtener la posición del objeto cargado (rocket)
+                    const rocketPosition = rocket.position.clone();
+
+                    // Añadir el objeto rocket a la escena
+                    scene.add(rocket);
+
+                    // Crear un nuevo objeto basado en la posición de rocket
+                    const zonePropulsion = new THREE.Mesh(
+                        new THREE.BoxGeometry(2, .5, 2),
+                        new THREE.MeshBasicMaterial({ color: 0xff0000 })
+                    );
+
+                    // Establecer la posición relativa de newObject respecto a rocket
+                    zonePropulsion.position.copy(rocketPosition);
+                    zonePropulsion.position.y -= -2; // Ajustar la posición en Y
+                    zonePropulsion.position.x -= 12; // Ajustar la posición en Y
+                    zonePropulsion.position.z -= -5; // Ajustar la posición en Z
+
+                    // Añadir el nuevo objeto a rocket como hijo
+                    rocket.add(zonePropulsion);
+                },
+                function (xhr) {
+                    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                },
+                function (error) {
+                    console.log('An error happened');
+                }
+            );
+        }
+    );
+
+    // Agregar luz ambiental
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    // Agregar luz direccional
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+
+    // Crear el renderizador
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Adjuntar el renderizador al elemento DOM correcto
+    const container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+
+    // Crear el controlador de cámara
+    cameraController = new CameraController(camera, container);
 }
 
-render();
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Actualizar el controlador de cámara
+    cameraController.update();
+
+    // Renderizar la escena
+    renderer.render(scene, camera);
+
+    console.log(camera.rotation)
+}
