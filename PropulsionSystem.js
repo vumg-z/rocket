35c336ef3f32997e3dc2038d4739b86373e48bc1
vumg <ab17100222@gmail.com>
2023-05-27 14:14:38 -0600

    commit bad

--- PropulsionSystem.js
+++ PropulsionSystem.js
@@ -1,118 +1,109 @@
 import * as THREE from 'three';
-import { OrbitControls } from './OrbitControls.js';
 import * as CANNON from 'cannon';
-let world, propulsionZoneBody, boxBodies = [];
 
-
-let camera, scene, renderer, controls;
-
-init();
-animate();
-
-function init() {
-    // Crear la cámara
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(-5.206487822512527, 0.3727371290852184, 20); // Ajustar la posición de la cámara
-    const target = new THREE.Vector3(0, 0, 0); // Establecer el punto al que la cámara apunta
-    camera.lookAt(target);
-
-    // Crear la escena
-    scene = new THREE.Scene();
-
-    // Crear el objeto propulsionZone
-    const propulsionZone = new THREE.Mesh(
-        new THREE.BoxGeometry(2, 0.5, 2),
-        new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    );
-    propulsionZone.position.set(0, 0, 0); // Establecer la posición de la zona de propulsión
-
-    // Añadir el objeto propulsionZone a la escena
-    scene.add(propulsionZone);
-
-    // Crear el renderizador
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Adjuntar el renderizador al elemento DOM correcto
-    const container = document.getElementById('container');
-    container.appendChild(renderer.domElement);
-
-    // Crear los controles de la cámara
-    controls = new OrbitControls(camera, renderer.domElement);
-
-    // Configurar el mundo físico
-    world = new CANNON.World();
-    world.gravity.set(0, -9.82, 0); // Gravedad en la dirección -y
-
-    // Crear la referencia de la posición de generación de las cajas
-    const boxGenerationPosition = new THREE.Vector3(0, -0.25, 0);
-
-    // Configurar cuerpo físico de propulsionZone
-    propulsionZoneBody = new CANNON.Body({
-        mass: 0, // mass == 0 hace que el cuerpo sea estático
-        position: new CANNON.Vec3(propulsionZone.position.x, propulsionZone.position.y, propulsionZone.position.z),
-        shape: new CANNON.Box(new CANNON.Vec3(1, 0.25, 1)) // los parámetros son la mitad de los tamaños en x, y, z
-    });
-    world.addBody(propulsionZoneBody);
-
-    // Crear y añadir cuerpos de caja justo debajo de la posición de generación
-    for (let i = 0; i < 10; i++) {
-        createBoxAtPosition(boxGenerationPosition.x, boxGenerationPosition.y, boxGenerationPosition.z);
-    }
-}
-
-// Función para crear una caja en una posición específica
-function createBoxAtPosition(x, y, z) {
-    let box = new THREE.Mesh(
-        new THREE.BoxGeometry(0.5, 0.5, 0.5),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    );
-    box.position.set(x + (Math.random() * 2 - 1), y, z + (Math.random() * 2 - 1)); // Posiciones aleatorias alrededor de la posición dada
-    scene.add(box);
-
-    let boxBody = new CANNON.Body({
-        mass: 1,
-        position: new CANNON.Vec3(box.position.x, box.position.y, box.position.z),
-        shape: new CANNON.Box(new CANNON.Vec3(0.25, 0.25, 0.25))
-    });
-    world.addBody(boxBody);
-    boxBodies.push({ threeObject: box, cannonBody: boxBody });
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    // Actualizar los controles de la cámara
-    controls.update();
-
-    // Avanzar la simulación física
-    world.step(1 / 60); // El argumento es el tiempo en segundos desde la última vez que se llamó a world.step
-
-    // Actualizar posiciones de los cuadros en Three.js para coincidir con Cannon.js
-    for (let box of boxBodies) {
-        box.threeObject.position.copy(box.cannonBody.position);
-        box.threeObject.quaternion.copy(box.cannonBody.quaternion);
+export default class PropulsionSimulator extends THREE.Object3D {
+    constructor(scene, rocket) {
+        super();
+        this.scene = scene;
+        this.world = new CANNON.World();
+        this.propulsionZoneBody = null;
+        this.boxBodies = [];
+        this.rocket = rocket;
+        this.rocketPosition = new THREE.Vector3();
+        this.rocketQuaternion = new THREE.Quaternion();
+        this.init();
     }
 
-    // Generar más cajas de forma continua justo debajo de la posición de generación
-    if (boxBodies.length < 100) { // Limitar el número total de cajas para evitar el agotamiento de recursos
-        createBoxAtPosition(0, -0.25, 0);
-    }
+    init() {
+        // Crear el objeto propulsionZone
+        const propulsionZone = new THREE.Mesh(
+            new THREE.BoxGeometry(2, 0.5, 2),
+            new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        );
+        propulsionZone.position.copy(this.position); // Establecer la posición de la zona de propulsión para coincidir con la posición del PropulsionSimulator
 
-    // Eliminar cajas que están por debajo de una cierta distancia
-    const distanceThreshold = -10; // Ajusta esto a la distancia que desees
-    for (let i = boxBodies.length - 1; i >= 0; i--) {
-        if (boxBodies[i].threeObject.position.y < distanceThreshold) {
-            // Eliminar de la escena Three.js
-            scene.remove(boxBodies[i].threeObject);
-            // Eliminar del mundo Cannon.js
-            world.remove(boxBodies[i].cannonBody);
-            // Eliminar de la lista boxBodies
-            boxBodies.splice(i, 1);
+        // Añadir el objeto propulsionZone a la escena
+        this.add(propulsionZone);
+
+        // Configurar el mundo físico
+        this.world = new CANNON.World();
+        this.world.gravity.set(0, -9.82, 0); // Gravedad en la dirección -y
+
+        // Configurar cuerpo físico de propulsionZone
+        this.propulsionZoneBody = new CANNON.Body({
+            mass: 0, // mass == 0 hace que el cuerpo sea estático
+            position: new CANNON.Vec3(this.position.x, this.position.y, this.position.z),
+            shape: new CANNON.Box(new CANNON.Vec3(1, 0.25, 1)) // los parámetros son la mitad de los tamaños en x, y, z
+        });
+        this.world.addBody(this.propulsionZoneBody);
+
+        // Crear y añadir cuerpos de caja justo debajo de la posición de generación
+        for (let i = 0; i < 10; i++) {
+            this.createBoxAtRandomPosition();
         }
     }
 
+    createBoxAtRandomPosition() {
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        );
+        const randomX = Math.random() * 2 - 1;
+        const randomZ = Math.random() * 2 - 1;
+        box.position.copy(this.position).add(new THREE.Vector3(randomX, -0.25, randomZ));
+        this.scene.add(box);
 
-    // Renderizar la escena
-    renderer.render(scene, camera);
+        const boxBody = new CANNON.Body({
+            mass: 1,
+            position: new CANNON.Vec3(box.position.x, box.position.y, box.position.z),
+            shape: new CANNON.Box(new CANNON.Vec3(0.25, 0.25, 0.25))
+        });
+
+        this.world.addBody(boxBody);
+        this.boxBodies.push({ threeObject: box, cannonBody: boxBody });
+
+        console.log('Box created at:', box.position.x, box.position.y, box.position.z); // Imprimir posición de la caja
+    }
+
+    updateRocketPosition() {
+        this.rocketPosition.copy(this.rocket.position);
+        this.rocketQuaternion.copy(this.rocket.quaternion);
+    }
+
+    animate() {
+        // Avanzar la simulación física
+        this.world.step(1 / 60);
+
+        // Actualizar posiciones y rotaciones de los cubos en Three.js para coincidir con Cannon.js
+        for (let box of this.boxBodies) {
+            const boxPositionRelativeToRocket = box.cannonBody.position.clone().vadd(this.position);
+            box.threeObject.position.copy(boxPositionRelativeToRocket);
+            box.threeObject.quaternion.copy(box.cannonBody.quaternion);
+        }
+
+        // Imprimir posición del cohete
+        console.log('Rocket position:', this.rocket.position.x, this.rocket.position.y, this.rocket.position.z);
+
+        // Rotar la zona de propulsión junto con el cohete
+        this.rotation.y += 0.01;
+
+        // Generar más cajas de forma continua justo debajo de la posición del cohete
+        if (this.boxBodies.length < 100) {
+            this.createBoxAtRandomPosition();
+        }
+
+        // Eliminar cajas que están por debajo de una cierta distancia
+        const distanceThreshold = -10;
+        for (let i = this.boxBodies.length - 1; i >= 0; i--) {
+            if (this.boxBodies[i].threeObject.position.y < distanceThreshold) {
+                this.scene.remove(this.boxBodies[i].threeObject);
+                this.world.remove(this.boxBodies[i].cannonBody);
+                this.boxBodies.splice(i, 1);
+            }
+        }
+    }
+
+    start() {
+        this.init();
+    }
 }
--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import * as THREE from 'three';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import CameraController from './camera';
+import PropulsionSimulator from './PropulsionSystem.js';
 
 let camera, scene, renderer, cameraController;
-
+let zonePropulsion;  // Variable global para mantener una referencia a zonePropulsion
+let rocketLoaded;
 
 init();
 animate();
@@ -12,10 +14,9 @@ animate();
 function init() {
     // Crear la cámara
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(-5.206487822512527, 0.3727371290852184, 14.501602931402532);
+    camera.position.set(-5.206487822512527, 0.3727371290852184, 40.501602931402532);
     const euler = new THREE.Euler(0, 0.5499999999999999, 0, 'XYZ');
     camera.setRotationFromEuler(euler);
-
 
     // Crear la escena
     scene = new THREE.Scene();
@@ -41,34 +42,32 @@ function init() {
                 // URL del archivo .obj
                 'rocket/rocket.obj',
                 function (rocket) {
-                    // Obtener la posición del objeto cargado (rocket)
-                    const rocketPosition = rocket.position.clone();
-
+                    rocketLoaded = rocket;
                     // Añadir el objeto rocket a la escena
-                    scene.add(rocket);
+                    scene.add(rocketLoaded);
 
-                    // Crear un nuevo objeto basado en la posición de rocket
-                    const zonePropulsion = new THREE.Mesh(
-                        new THREE.BoxGeometry(2, .5, 2),
-                        new THREE.MeshBasicMaterial({ color: 0xff0000 })
-                    );
+                    // Crear un nuevo objeto Propulsion system basado en la posición del cohete
+                    zonePropulsion = new PropulsionSimulator(scene, rocket);
 
-                    // Establecer la posición relativa de newObject respecto a rocket
-                    zonePropulsion.position.copy(rocketPosition);
-                    zonePropulsion.position.y -= -2; // Ajustar la posición en Y
-                    zonePropulsion.position.x -= 12; // Ajustar la posición en Y
-                    zonePropulsion.position.z -= -5; // Ajustar la posición en Z
+                    // ...
 
-                    // Añadir el nuevo objeto a rocket como hijo
-                    rocket.add(zonePropulsion);
+                    zonePropulsion.start(); // Llamar al método start() en PropulsionSimulator
                 },
                 function (xhr) {
                     console.log((xhr.loaded / xhr.total * 100) + '% loaded');
                 },
                 function (error) {
                     console.log('An error happened');
+                    console.log(error);
                 }
             );
+
+            // ...
+
+
+            // ...
+
+
         }
     );
 
@@ -99,7 +98,17 @@ function animate() {
     // Actualizar el controlador de cámara
     cameraController.update();
 
+    // Actualizar la simulación de propulsión, si ha sido inicializada
+    if (zonePropulsion) {
+        zonePropulsion.updateRocketPosition(rocketLoaded); // Actualizar la posición y la rotación del cohete
+        zonePropulsion.animate(); // Llamar al método animate() en PropulsionSimulator
+    }
+
+    if (rocketLoaded) {
+        rocketLoaded.rotation.y += 0.01;
+    }
+
     // Renderizar la escena
     renderer.render(scene, camera);
-
 }
+
